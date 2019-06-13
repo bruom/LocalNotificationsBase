4ad0ff5f410eb47b8b506a6e5a4159ef6e866f5d
Guilherme Enes <guilherme.epecci@gmail.com>
2019-06-13 18:11:26 -0300

    enes bronze e prata

--- LocalNotificationBase/BaseViewController.swift
+++ LocalNotificationBase/BaseViewController.swift
@@ -9,24 +9,115 @@
 import UIKit
 import UserNotifications
 
-class BaseViewController: UIViewController {
+class BaseViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UNUserNotificationCenterDelegate {
+    
+    
+    @IBOutlet weak var Sound: UISwitch!
+    @IBOutlet weak var Badge: UISwitch!
+    @IBOutlet weak var NotificationTitle: UITextField!
+    @IBOutlet weak var NotificationInfo: UITextField!
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    } // numero de colunas
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count // numero de elementos dentro do vetor
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row] // se refere a um elemento especifico do vetor
+    }
+    
 
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var pickerData: [String] = [String]() // cria o vetor
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        pickerData = ["1","2","3","4","5","6","7","8","9"] // numero de elementos
+        configureUserNotificationsCenter()
+        
+    }
+    
+    private func configureUserNotificationsCenter() {
+        // Configure User Notification Center
+        UNUserNotificationCenter.current().delegate = self
+        let actionShowDetails = UNNotificationAction(identifier: Notification.Action.showDetails, title: "Show Details", options: [.foreground])
+        let actionRepete = UNNotificationAction(identifier: Notification.Action.repete, title: "Repeat", options: [])
+        let tutorialCategory = UNNotificationCategory(identifier: Notification.Category.tutorial, actions: [actionRepete, actionShowDetails], intentIdentifiers: [], options: [])
+        
+        UNUserNotificationCenter.current().setNotificationCategories([tutorialCategory])
     }
 
-    @IBAction func remindButton(_ sender: Any) {
+    struct Notification {
+        struct Category {
+            static let tutorial = "tutorial"
+        }
+        struct Action {
+            static let repete = "repete"
+            static let showDetails = "showDetails"
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        switch response.actionIdentifier {
+        case Notification.Action.repete:
+            CreateNotification()
+            
+        case Notification.Action.showDetails:
+            performSegue(withIdentifier: "segue", sender: nil)
+            
+            
+        default:
+            print("Other Action")
+        }
+        
+        completionHandler()
+    }
+    
+    func CreateNotification () {
         let notificationCenter = UNUserNotificationCenter.current()
         notificationCenter.getNotificationSettings { (settings) in
             if settings.authorizationStatus == .authorized {
                 
                 let content = UNMutableNotificationContent()
-                content.title = NSString.localizedUserNotificationString(forKey: "Lembre-se", arguments: nil)
-                content.body = NSString.localizedUserNotificationString(forKey: "Você se lembrou", arguments: nil)
-                content.sound = UNNotificationSound.default
+                if self.NotificationTitle.text != nil {
+                    content.title = NSString.localizedUserNotificationString(forKey: self.NotificationTitle.text!, arguments: nil)
+                } else {
+                    self.NotificationTitle.text = ""
+                    content.title = NSString.localizedUserNotificationString(forKey: self.NotificationTitle.text!, arguments: nil)
+                }
                 
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                if self.NotificationInfo.text != nil {
+                    content.body = NSString.localizedUserNotificationString(forKey: self.NotificationInfo.text!, arguments: nil)
+                } else {
+                    self.NotificationInfo.text = ""
+                    content.body = NSString.localizedUserNotificationString(forKey: self.NotificationInfo.text!, arguments: nil)
+                }
+                
+                if self.Sound.isOn {
+                    content.sound = UNNotificationSound.default
+                } else {
+                    print("n vai ter som")
+                }
+                
+                if self.Badge.isOn {
+                    content.badge = 1
+                } else {
+                    print("Nada ocorre")
+                }
+                
+                content.categoryIdentifier = Notification.Category.tutorial      // usa da categoria Tutorial
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: Double(self.pickerData[self.picker.selectedRow(inComponent: 0)]) as! TimeInterval, repeats: false)
                 
                 let request = UNNotificationRequest(identifier: "5seconds", content: content, trigger: trigger)
                 
@@ -43,5 +134,14 @@ class BaseViewController: UIViewController {
         }
     }
     
+    @IBAction func remindButton(_ sender: Any) {
+        CreateNotification()
+    }
 }
+
+    
+    
+
+    
+
 
---
+++ LocalNotificationBase/TimeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TimeViewController.swift
+//  LocalNotificationBase
+//
+//  Created by Guilherme Enes on 13/06/19.
+//  Copyright © 2019 Bruno Omella Mainieri. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TimeViewController: UIViewController, UIPickerViewDelegate{
+    
+}
