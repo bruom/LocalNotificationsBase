fefdd79b3e9a242e1a38e7b237b59f87b0143221
Galdineris <galdineris@gmail.com>
2019-06-13 18:36:25 -0300

    Bronze e Prata Challenge Feitos

--- LocalNotificationBase/BaseViewController.swift
+++ LocalNotificationBase/BaseViewController.swift
@@ -9,26 +9,62 @@
 import UIKit
 import UserNotifications
 
-class BaseViewController: UIViewController {
+class BaseViewController: UITableViewController, UITextFieldDelegate, UNUserNotificationCenterDelegate{
 
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextField: UITextField!
+    @IBOutlet weak var triggerTextField: UITextField!
+    @IBOutlet weak var soundSwitch: UISwitch!
+    @IBOutlet weak var badgeSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let tap = UITapGestureRecognizer(target: self.view,
+                                         action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+        
     }
+    
+    @IBAction func copperButton(_ sender: Any) {
+        
+        var title : String = self.titleTextField.text ?? "Recuerda-me"
+        title = title.isEmpty ? "Recuerda-me" : title
+        var body : String = self.bodyTextField.text ?? "Aunque Tenga Que Emigrar"
+        body = body.isEmpty ? "Aunque Tenga Que Emigrar" : body
+        let triggerTime : Double = (self.triggerTextField.text != nil) ? Double(self.triggerTextField.text!) ?? 5 : 5
 
-    @IBAction func remindButton(_ sender: Any) {
+        
+        let sound : Bool = self.soundSwitch.isOn
+        let badge : Bool = self.badgeSwitch.isOn
+        
         let notificationCenter = UNUserNotificationCenter.current()
         notificationCenter.getNotificationSettings { (settings) in
             if settings.authorizationStatus == .authorized {
                 
                 let content = UNMutableNotificationContent()
-                content.title = NSString.localizedUserNotificationString(forKey: "Lembre-se", arguments: nil)
-                content.body = NSString.localizedUserNotificationString(forKey: "Você se lembrou", arguments: nil)
-                content.sound = UNNotificationSound.default
+                content.title = NSString.localizedUserNotificationString(forKey: title,
+                                                                         arguments: nil)
+                content.body = NSString.localizedUserNotificationString(forKey: body,
+                                                                        arguments: nil)
                 
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                if sound{
+                    content.sound = UNNotificationSound.default
+                }
                 
-                let request = UNNotificationRequest(identifier: "5seconds", content: content, trigger: trigger)
+                if badge{
+                    content.badge = 1
+                }
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: triggerTime,
+                                                                repeats: false)
+                
+                let request = UNNotificationRequest(identifier: "copper",
+                                                    content: content,
+                                                    trigger: trigger)
                 
                 let center = UNUserNotificationCenter.current()
                 center.add(request) { (error : Error?) in
@@ -43,5 +79,90 @@ class BaseViewController: UIViewController {
         }
     }
     
+    @IBAction func silverButton(_ sender: Any) {
+        silverNotification()
+    }
+    @IBAction func goldButton(_ sender: Any) {
+        silverNotification()
+    }
+    
+    func silverNotification(){
+        var title : String = self.titleTextField.text ?? "Recuerda-me"
+        title = title.isEmpty ? "Recuerda-me" : title
+        var body : String = self.bodyTextField.text ?? "Aunque Tenga Que Emigrar"
+        body = body.isEmpty ? "Aunque Tenga Que Emigrar" : body
+        let triggerTime : Double = (self.triggerTextField.text != nil) ? Double(self.triggerTextField.text!) ?? 5 : 5
+        
+        
+        let sound : Bool = self.soundSwitch.isOn
+        let badge : Bool = self.badgeSwitch.isOn
+        
+        let repeatAction = UNNotificationAction(identifier: "repeatAction",
+                                                title: "Repeat",
+                                                options: [.foreground])
+        let openAction = UNNotificationAction(identifier: "openAction",
+                                              title: "Let's Go",
+                                              options: [.foreground])
+        
+        let silverCategory = UNNotificationCategory(identifier: "silverCategory",
+                                                    actions: [repeatAction, openAction],
+                                                    intentIdentifiers: [],
+                                                    options: [])
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.delegate = self
+        notificationCenter.setNotificationCategories([silverCategory])
+        notificationCenter.getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .authorized {
+                
+                let content = UNMutableNotificationContent()
+                content.title = NSString.localizedUserNotificationString(forKey: title,
+                                                                         arguments: nil)
+                content.body = NSString.localizedUserNotificationString(forKey: body,
+                                                                        arguments: nil)
+                content.categoryIdentifier = "silverCategory"
+                
+                if sound{
+                    content.sound = UNNotificationSound.default
+                }
+                
+                if badge{
+                    content.badge = 1
+                }
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: triggerTime,
+                                                                repeats: false)
+                
+                let request = UNNotificationRequest(identifier: "copper",
+                                                    content: content,
+                                                    trigger: trigger)
+                
+                let center = UNUserNotificationCenter.current()
+                center.add(request) { (error : Error?) in
+                    if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+                
+            } else {
+                print("Impossível mandar notificação - permissão negada")
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        switch response.actionIdentifier {
+        case "repeatAction":
+            silverNotification()
+            break
+        case "openAction":
+            performSegue(withIdentifier: "silverSegue", sender: self)
+        default:
+            print("Notifications Actions Not Working Properly")
+        }
+    }
+    
+    
 }
-
