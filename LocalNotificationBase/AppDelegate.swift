aa419df75c3537090f307ffabeff45c39799697a
Mariana Lima <mrl.lima85@gmail.com>
2019-06-13 18:24:34 -0300

    mariana lima bronze

--- LocalNotificationBase/AppDelegate.swift
+++ LocalNotificationBase/AppDelegate.swift
@@ -11,7 +11,7 @@ import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+ 
     var window: UIWindow?
 
 
---
+++ LocalNotificationBase/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  LocalNotificationBase
+//
+//  Created by Mariana Lima on 13/06/19.
+//  Copyright © 2019 Bruno Omella Mainieri. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
